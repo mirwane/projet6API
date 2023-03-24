c9971b1686f93ad41bebe51d913e95c4adf0accb
marwan <mir1_93@hotmail.com>
2023-03-24 15:36:19 +0100

    24

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
 const app = express();
+const cors = require("cors");
 const port = 3000;
 
 // Database
