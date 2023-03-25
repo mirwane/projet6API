20bb1581af1ec071e031662cc9edfb5a4bf829ae
marwan <mir1_93@hotmail.com>
2023-03-25 14:30:21 +0100

    hashedpasseword ok

--- backend/users.js
+++ backend/users.js
@@ -1,13 +1,24 @@
 const { User } = require("./mongo");
+const bcrypt = require("bcrypt");
 
-function createUser(req, res) {
+async function createUser(req, res) {
   const { email, password } = req.body;
-  const user = new User({ email, password });
+
+  const hashedPassword = await hashPassword(password);
+  console.log("password: ", password);
+  console.log("hashedPassword: ", hashedPassword);
+
+  const user = new User({ email, password: hashedPassword });
 
   user
     .save()
     .then(() => res.send("utilisateur enregistré"))
     .catch((err) => console.log("utilisateur non enregistré", err));
+}
+
+function hashPassword(password) {
+  const saltRounds = 10;
+  return bcrypt.hash(password, saltRounds);
 }
 
 module.exports = { createUser };
