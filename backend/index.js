6884bc5cfb7da3ed878bbe476fa22be277aa5e74
marwan <mir1_93@hotmail.com>
2023-04-11 15:31:41 +0200

    11/04

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,36 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const cors = require("cors");
+const { app, express } = require("./server");
 const port = 3000;
+const path = require("path");
 
 // Database
 require("./mongo");
 
 // Controllers
 const { createUser, logUser } = require("./users");
-const { getSauces, createSauces } = require("./sauces");
+const {
+  getSauces,
+  getSauceById,
+  createSauces,
+  deleteSauce,
+} = require("./sauces");
+
 // Middleware
-app.use(cors());
-app.use(express.json());
-const { authentificatUser } = require("./auth");
+const { upload } = require("./middleware/mulder");
+const { authentificatUser } = require("./middleware/auth");
 
 // Routes
 app.post("/api/auth/signup", createUser);
 app.post("/api/auth/login", logUser);
 app.get("/api/sauces", authentificatUser, getSauces);
-app.post("/api/sauces", authentificatUser, createSauces);
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.post(
+  "/api/sauces",
+  authentificatUser,
+  upload.single("image"),
+  createSauces
+);
+app.get("/api/sauces/:id", authentificatUser, getSauceById);
+app.delete("/api/sauces/:id", authentificatUser, deleteSauce);
 
 // Listen
+app.use("/images", express.static(path.join(__dirname, "images")));
 app.listen(port, () => console.log("listening on port " + port));
--- backend/sauces.js
+++ backend/sauces.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const unlink = require("fs").promises.unlink;
+
 const productSchema = new mongoose.Schema({
   userId: String,
   name: String,
@@ -15,30 +17,59 @@ const productSchema = new mongoose.Schema({
 const Product = mongoose.model("Product", productSchema);
 
 function getSauces(req, res) {
-  console.log("getSauces");
-  // console.log("token valid", decoded);
-  Product.find({}).then((products) => res.send({ products }));
-  // res.send({ message: [{ sauce: "sauce1" }, { sauce: "sauce2" }] });
+  Product.find({}).then((products) => res.send(products));
 }
 
-function createSauces(req, res) {
-  const product = new Product({
-    userId: req.body.userId,
-    name: req.body.name,
-    manufacturer: req.body.manufacturer,
-    description: req.body.description,
-    mainPepper: req.body.mainPepper,
-    imageUrl: req.body.imageUrl,
-    heat: req.body.heat,
-    likes: req.body.likes,
-    dislikes: req.body.dislikes,
-    usersLiked: req.body.usersLiked,
-    usersDisliked: req.body.usersDisliked,
-  });
-  product
-    .save()
-    .then((res) => console.log("sauce saved", res))
+function getSauceById(req, res) {
+  const { id } = req.params;
+  Product.findById(id)
+    .then((product) => res.send(product))
     .catch((err) => console.log(err));
 }
 
-module.exports = { createSauces, getSauces };
+function createSauces(req, res) {
+  const { body, file } = req;
+  const { fileName } = file;
+  const sauce = JSON.parse(body.sauce);
+  const { userId, name, manufacturer, description, mainPepper, heat } = sauce;
+  function makeImageURL(req, file) {
+    return req.protocol + "://" + req.get("host") + "/images/" + file.filename;
+  }
+  const product = new Product({
+    userId: userId,
+    name: name,
+    manufacturer: manufacturer,
+    description: description,
+    mainPepper: mainPepper,
+    imageUrl: makeImageURL(req, file),
+    heat: heat,
+    likes: 0,
+    dislikes: 0,
+    usersLiked: [0],
+    usersDisliked: [0],
+  });
+  product
+    .save()
+    .then((message) => {
+      res.status(201).send({ message: message });
+      return console.log("sauce saved", message);
+    })
+    .catch((err) => console.log(err));
+}
+
+function deleteSauce(req, res) {
+  const { id } = req.params;
+
+  Product.findByIdAndDelete(id)
+    .then(deleteImage)
+    .then((product) => res.status(200).send({ message: product }))
+    .catch((err) => res.status(500).send({ message: err }));
+}
+
+function deleteImage(product) {
+  const { imageUrl } = product;
+  const filename = imageUrl.split("/images/")[1];
+  return unlink(`images/${filename}`);
+}
+
+module.exports = { getSauces, getSauceById, createSauces, deleteSauce };
