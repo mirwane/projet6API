1fcb9498c1f627a845d976e8fe22a2af0bdfb696
marwan <mir1_93@hotmail.com>
2023-04-25 09:37:41 +0100

    25

--- backend/sauces.js
+++ backend/sauces.js
@@ -112,22 +112,51 @@ function deleteImage(product) {
   return unlink(`images/${filename}`).then(() => product);
 }
 function likeSauces(req, res) {
-  const {like, useId } = req.body
-  if (![1, -1, 0].includes(like)) return res.status(403).send({ message: "invalid"})
-  
+  const { like, useId } = req.body;
+  if (![1, -1, 0].includes(like))
+    return res.status(403).send({ message: "invalid" });
+
   getsauce(req, res)
-  .then((product) => updateLike(product, like, userId))
-  .catch((err) => res.status(500).send(err))
+    .then((product) => updateVote(product, like, userId, res))
+    .then((pro) => pro.save())
+    .then((prod = sendClientResponse(prod, res)))
+    .catch((err) => res.status(500).send(err));
+}
 
-function updateVote(product, like, userId) {
-  if (like --- 1 ) incrementLike(product, userId)
-} 
+function updateVote(product, like, userId, res) {
+  if (like === 1 || like === -1) return incrementVote(product, userId);
+  return resetVote(product, userId, res);
+}
 
-function incrementLike(product, userId){
-const {userLiked } = product
-if (userLiked.includes(userId)) return
-userLiked.push(userId) 
-porudct.likes++
+function resetVote(product, userId, res) {
+  const { userLiked, userDisliked } = product;
+  if ([userLiked, userDisliked].every((arr) => arr.includes(userId)))
+    return Promise.reject("User has voted");
+
+  if (![userLiked, userDisliked].some((arr) => arr.includes(userId)))
+    return Promise.reject("User has not voted");
+
+  if (userLiked.includes(userId)) {
+    --product.likes;
+    product.userLiked = product.userLiked.filter((id) => id !== userId);
+  } else {
+    --product.dislikes;
+    product.userDisliked = product.userDisliked.filter((id) => id !== userId);
+  }
+
+  let arrayToUpdate = userLiked.includes(userId) ? userLiked : userDisliked;
+  arrayToUpdate = arrayToUpdate.filter((id) => id !== userId);
+  return product;
+}
+
+function incrementVote(product, userId, like) {
+  const { userLiked, userDisliked } = product;
+  const voteArray = like === 1 ? userLiked : userDisliked;
+  if (voteArray.includes(userId)) return product;
+  voteArray.push(userId);
+
+  like === 1 ? ++product.likes : ++product.dislikes;
+  return product;
 }
 
 module.exports = {
@@ -137,4 +166,4 @@ module.exports = {
   deleteSauce,
   modifySauce,
   likeSauces,
-}
+};
